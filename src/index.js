e4246432d76107edaa0233458094f49c3c544c7e
cygnus755 <mohit.ranjan.910@gmail.com>
2022-10-01 18:54:15 +0530

    Added check for input file does not exist

--- src/index.js
+++ src/index.js
@@ -15,18 +15,24 @@ if (!process.argv[2]) {
 const inputFile = process.argv[2];
 
 try {
-    // Read the input file synchronously
-    const data = fs.readFileSync(inputFile, 'utf-8');
-    // get all the tokens in the source code
-    let tokens = tokenizer(data);
-    // now it's time to generate AST for the program
-    let indexObject = {
-        value : 0
+    // check if the file exists, and if yes then proceed ahead
+    if (fs.existsSync(inputFile)) {
+        // Read the input file synchronously
+        const data = fs.readFileSync(inputFile, 'utf-8');
+        // get all the tokens in the source code
+        let tokens = tokenizer(data);
+        // now it's time to generate AST for the program
+        let indexObject = {
+            value : 0
+        }
+        let abstractSyntaxTree = parseProgram(tokens, indexObject);
+        // generating the code
+        const fileName = path.basename(inputFile).split('.')[0];
+        codeGenerator(abstractSyntaxTree, fileName);
+    } else {
+        console.log(`File ${inputFile} does not exist!`);
+        exit(1);
     }
-    let abstractSyntaxTree = parseProgram(tokens, indexObject);
-    // generating the code
-    const fileName = path.basename(inputFile).split('.')[0];
-    codeGenerator(abstractSyntaxTree, fileName);
 } catch (err) {
     console.log(err);
 }
